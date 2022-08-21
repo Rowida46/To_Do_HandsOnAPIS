b0ff53cd826227f5f4926f4684ea21fce3dec38d
Rowida <rowidanagah462000@gmail.com>
2022-08-22 00:36:16 +0200

    adding db & context

---
+++ To_Do_HandsOnAPIS/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace To_Do_HandsOnAPIS.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options
+            ) : base(options){}
+
+       DbSet<ToDoItem> ToDoItems { get; set; }
+
+
+    }
+}
